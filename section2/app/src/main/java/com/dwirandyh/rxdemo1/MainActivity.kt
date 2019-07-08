6f4af4c0861f2c804178554a4b87f5ce59620e1d
Dwi Randy H <dwirandyherdinanto@gmail.com>
2019-07-08 11:52:07 +0700

    Disposable

--- section2/app/src/main/java/com/dwirandyh/rxdemo1/MainActivity.kt
+++ section2/app/src/main/java/com/dwirandyh/rxdemo1/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.util.Log
 import io.reactivex.Observable
 import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,11 +17,20 @@ class MainActivity : AppCompatActivity() {
     lateinit var observable: Observable<String>
     lateinit var myObserver: Observer<String>
 
+    lateinit var disposable: Disposable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         observable = Observable.just(greeting)
+
+        // observable schedulers.io
+        observable.subscribeOn(Schedulers.io())
+
+        // observable schedulers mainThread/UIThread
+        observable.observeOn(AndroidSchedulers.mainThread())
+
         myObserver = getObserver()
 
         observable.subscribe(myObserver)
@@ -29,10 +40,12 @@ class MainActivity : AppCompatActivity() {
         val mySubscriber = object : Observer<String> {
             override fun onComplete() {
                 Log.i(TAG, "onComplete Invoked")
+
             }
 
             override fun onSubscribe(d: Disposable) {
                 Log.i(TAG, "onSubscribe Invoked")
+                disposable = d
             }
 
             override fun onNext(t: String) {
@@ -46,5 +59,10 @@ class MainActivity : AppCompatActivity() {
 
         }
         return mySubscriber
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposable.dispose()
     }
 }
